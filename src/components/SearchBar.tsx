f2df331825ae883f6da0a8bed205e8331c0571f7
Himalian <Voltage15312@outlook.com>
2026-03-27 18:15:25 +0800

    feat: new search-bar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,6 +5,7 @@ import { locationService } from '../services';
 import { memoSpecialTypes } from '../helpers/filter';
 import '../less/search-bar.less';
 import Search from '../icons/search.svg?component';
+// import { Search } from 'lucide-react';
 import { t } from '../translations/helper';
 import useToggle from '../hooks/useToggle';
 
@@ -83,45 +84,64 @@ const SearchBar: React.FC<Props> = () => {
   return (
     // <div className={`${isSearchBarShown ? "search-bar-container-long" : "search-bar-container-short"}`}>
     //   <div className={`${isSearchBarShown ? "search-bar-inputer-long" : "search-bar-inputer-short"}`}>
-    <div className="search-bar-container">
-      <div className="search-bar-inputer">
-        {/*<img className="icon-img" src={search} />*/}
-        <Search className="icon-img" />
-        <input
-          className="text-input"
-          type="text"
-          onMouseOver={mouseIn}
-          onMouseOut={mouseOut}
-          placeholder={isSearchBarShow ? 'Type Here' : ''}
-          onChange={handleTextQueryInput}
-        />
-      </div>
-      <div className="quickly-action-wrapper">
-        <div className="quickly-action-container">
-          <p className="title-text">{t('Quick filter')}</p>
-          <div className="section-container types-container">
-            <span className="section-text">{t('TYPE')}:</span>
-            <div className="values-container">
-              {memoSpecialTypes.map((t, idx) => {
-                return (
-                  <div key={t.value}>
-                    <span
-                      className={`type-item ${memoType === t.value ? 'selected' : ''}`}
-                      onClick={() => {
-                        handleMemoTypeItemClick(t.value as MemoSpecType);
-                      }}
-                    >
-                      {t.text}
-                    </span>
-                    {idx + 1 < memoSpecialTypes.length ? <span className="split-text">/</span> : null}
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </div>
-      </div>
+    <div
+      className="search-bar-container h-8 text-sm w-auto flex flex-row justify-self-start
+      items-center bg-transparent p-1"
+    >
+      <Search className="shrink-0 w-4 h-4 opacity-60"></Search>
+      <input
+        name="search"
+        className="flex-1 min-w-0 bg-transparent ml-0.5 px-0"
+        type="text"
+        placeholder="Search Memos"
+        onMouseOver={mouseIn}
+        onMouseOut={mouseOut}
+        onFocus={mouseIn}
+        onBlur={mouseOut}
+        onChange={handleTextQueryInput}
+      ></input>
     </div>
+    // <div className="search-bar-container">
+    //   <div className="search-bar-inputer">
+    //     {/*<img className="icon-img" src={search} />*/}
+    //     <Search className="icon-img" />
+    //     <input
+    //       className="text-input"
+    //       type="text"
+    //       onMouseOver={mouseIn}
+    //       onMouseOut={mouseOut}
+    //       onFocus={mouseIn}
+    //       onBlur={mouseOut}
+    //       placeholder={isSearchBarShow ? 'Type At Here' : ''}
+    //       onChange={handleTextQueryInput}
+    //     />
+    //   </div>
+    //   <div className="quickly-action-wrapper">
+    //     <div className="quickly-action-container">
+    //       <p className="title-text">{t('Quick filter')}</p>
+    //       <div className="section-container types-container">
+    //         <span className="section-text">{t('TYPE')}:</span>
+    //         <div className="values-container">
+    //           {memoSpecialTypes.map((t, idx) => {
+    //             return (
+    //               <div key={t.value}>
+    //                 <span
+    //                   className={`type-item ${memoType === t.value ? 'selected' : ''}`}
+    //                   onClick={() => {
+    //                     handleMemoTypeItemClick(t.value as MemoSpecType);
+    //                   }}
+    //                 >
+    //                   {t.text}
+    //                 </span>
+    //                 {idx + 1 < memoSpecialTypes.length ? <span className="split-text">/</span> : null}
+    //               </div>
+    //             );
+    //           })}
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
